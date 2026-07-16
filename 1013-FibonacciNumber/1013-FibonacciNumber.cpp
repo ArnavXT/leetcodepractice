8afee18fca0f9bdfc85ca5d3566dd178b6384a5e
Arnav Tripathi <arnavtripathi21045@gmail.com>
2026-07-16 16:37:42 +0530

    Create 1013-FibonacciNumber

---
+++ 1013-FibonacciNumber/1013-FibonacciNumber.cpp
@@ -0,0 +1,9 @@
+// Last updated: 7/16/2026, 4:37:39 PM
+class Solution {
+public:
+    int fib(int n) {
+       if(n==0) return 0;
+       if(n==1) return 1;
+       return fib(n-1)+fib(n-2); 
+    }
+};
