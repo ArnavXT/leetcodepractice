6a8b9db0b5e3a9114955cd7fe4ebf3d4f183078f
Arnav Tripathi <arnavtripathi21045@gmail.com>
2026-07-16 16:38:44 +0530

    Create 151-ReverseWordsInAString

---
+++ 151-ReverseWordsInAString/151-ReverseWordsInAString.cpp
@@ -0,0 +1,24 @@
+// Last updated: 7/16/2026, 4:38:42 PM
+class Solution {
+public:
+    string reverseWords(string s) {
+        reverse(s.begin(), s.end());
+        int n = s.size();
+        int l = 0;
+        int r = 0;
+        int i = 0; 
+        while(i<n){
+            while(i<n && s[i] == ' ')
+            i++;
+            if(i == n) break;
+            while(i<n && s[i] != ' ')
+            s[r++] = s[i++];
+            reverse(s.begin() +l , s.begin() + r);
+            s[r++] = ' ';
+            l = r;
+            i++;
+        }
+            s.resize(r - 1);
+            return s;
+    }
+};
