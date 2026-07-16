984d58e5fcaa2284d31565c44433f7ab5488c34b
Arnav Tripathi <arnavtripathi21045@gmail.com>
2026-07-16 16:37:02 +0530

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,15 @@
+// Last updated: 7/16/2026, 4:36:59 PM
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    return function(...args) {
+        return ("Hello World");
+        
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
