c4234218303b1ffd7ea14c6eaa2ffea38ebf8927
Arnav Tripathi <arnavtripathi21045@gmail.com>
2026-07-16 16:39:02 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.cpp
@@ -0,0 +1,24 @@
+// Last updated: 7/16/2026, 4:39:00 PM
+class Solution {
+public:
+    int t[46];
+    
+    int solve(int n){
+        if(n < 0)
+            return 0;
+        if(n == 0)
+            return 1;
+        if(t[n] != -1)
+            return t[n];
+        
+        int one = solve(n-1);
+        int two = solve(n-2);
+
+        return t[n] = one + two;
+    }
+    
+    int climbStairs(int n) {
+        memset(t, -1, sizeof(t));
+        return solve(n);
+    }
+};
