97610df192f8d237e23bde2c21d0bcc08210558d
Arnav Tripathi <arnavtripathi21045@gmail.com>
2026-07-17 21:24:41 +0530

    File updated at 7/17/2026, 9:24:39 PM

--- 169-MajorityElement/169-MajorityElement.cpp
+++ 169-MajorityElement/169-MajorityElement.cpp
@@ -1,28 +1,9 @@
-// Last updated: 7/16/2026, 4:38:39 PM
-class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
-        if(nums.size() < 2){
-            return nums[0];
-        }
-        int maj = (nums.size() + 2 - 1) / 2;
-
-        sort(nums.begin(), nums.end());
-        int count = 1;
-        int final = 0;
-
-        for(int i = 1; i < nums.size(); i++){
-            if(nums[i] == nums[i - 1]){
-                cout << nums[i] << " " << count << endl;
-                count += 1;
-            }else{
-                count = 1;
-            }
-            if(count >= maj){
-                final = nums[i];
-                break;   
-            }    
-        }
-        return final;
-    }
-};
+// Last updated: 7/17/2026, 9:24:39 PM
+1class Solution {
+2public:
+3    int majorityElement(vector<int>& nums) {
+4        sort(nums.begin(), nums.end());
+5        int n =  nums.size();
+6        return nums[n/2];
+7    }
+8};
