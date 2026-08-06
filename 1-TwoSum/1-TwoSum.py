9dda36799e9fa5be582a32091fa88513fc740471
Arnav Tripathi <arnavtripathi21045@gmail.com>
2026-08-06 20:01:39 +0530

    File updated at 8/6/2026, 8:01:38 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,12 +1,10 @@
-# Last updated: 7/16/2026, 4:39:48 PM
-class Solution(object):
-    def twoSum(self, num, target):
-        for i in range(len(num)):
-            for j in range(i+1,len(num)):
-                if num[i]+num[j] == target:
-                     return [i,j]
-
-solution = Solution()
-num =[1,2,4,7,8,3]
-target = 9
-print(solution.twoSum(num,target))
+# Last updated: 8/6/2026, 8:01:38 PM
+1class Solution:
+2    def twoSum(self, nums: List[int], target: int) -> List[int]:
+3        L = {}
+4        for i in range(len(nums)):
+5            rem = target - nums[i]
+6            if rem in L:
+7                return [L[rem], i]
+8            L[nums[i]] = i 
+9
