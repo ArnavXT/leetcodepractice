aa1c5be290e709be2d218673e8cad40db049386f
Arnav Tripathi <arnavtripathi21045@gmail.com>
2026-08-20 15:51:52 +0530

    Create 3069-DistributeElementsIntoTwoArraysI

---
+++ 3069-DistributeElementsIntoTwoArraysI/3069-DistributeElementsIntoTwoArraysI.py
@@ -0,0 +1,8 @@
+# Last updated: 8/20/2026, 3:51:51 PM
+1class Solution:
+2    def resultArray(self, nums: List[int]) -> List[int]:
+3        A=([nums[0]], [nums[1]])
+4        for x in nums[2:]:
+5            A[A[0][-1]<=A[1][-1]].append(x)
+6        return A[0]+A[1]
+7        
