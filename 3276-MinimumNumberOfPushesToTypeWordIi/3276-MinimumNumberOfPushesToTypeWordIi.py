ca8e7442ada855637d08a92d31b26214b8e0ee57
Arnav Tripathi <arnavtripathi21045@gmail.com>
2026-08-26 09:59:47 +0530

    Create 3276-MinimumNumberOfPushesToTypeWordIi

---
+++ 3276-MinimumNumberOfPushesToTypeWordIi/3276-MinimumNumberOfPushesToTypeWordIi.py
@@ -0,0 +1,27 @@
+# Last updated: 8/26/2026, 9:59:46 AM
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        freq = [0] * 26
+
+        for ch in word:
+            freq[ord(ch) - ord('a')] += 1
+
+        ans = 0
+        assigned = 0
+
+        while True:
+            mx = 0
+            idx = -1
+
+            for i in range (26):
+                if freq[i] > mx:
+                    mx = freq[i]
+                    idx = i   
+
+            if idx == -1: 
+                break
+
+            ans += mx * (assigned // 8 + 1)
+            freq[idx] = 0
+            assigned += 1
+        return ans
