44ced215598412c5a44c6e1d05ee920deedbf52a
Arnav Tripathi <arnavtripathi21045@gmail.com>
2026-09-06 12:30:55 +0530

    Create 1512-NumberOfGoodPairs

---
+++ 1512-NumberOfGoodPairs/1512-NumberOfGoodPairs.cpp
@@ -0,0 +1,18 @@
+// Last updated: 9/6/2026, 12:30:54 PM
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int count = 0;
+        int n = nums.size();
+
+        for (int i = 0;i<n;i++){
+            for (int j = 0;j<n;j++){
+                if(i<j && nums[i]==nums[j]){
+                    count++;
+                }
+            }
+        }
+        return count++;    
+        
+    }
+};
