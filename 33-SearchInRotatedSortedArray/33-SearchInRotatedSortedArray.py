f8a6ced1d110f26efbad74cf60ac3695953520ff
Arnav Tripathi <arnavtripathi21045@gmail.com>
2026-09-06 12:34:10 +0530

    Create 33-SearchInRotatedSortedArray

---
+++ 33-SearchInRotatedSortedArray/33-SearchInRotatedSortedArray.py
@@ -0,0 +1,7 @@
+# Last updated: 9/6/2026, 12:34:09 PM
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i
+        return -1
