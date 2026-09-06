e0c65d74c23e0c137376785fa40a439b2fb61ed2
Arnav Tripathi <arnavtripathi21045@gmail.com>
2026-09-06 12:28:47 +0530

    Create 3788-MaximumScoreOfASplit

---
+++ 3788-MaximumScoreOfASplit/3788-MaximumScoreOfASplit.cpp
@@ -0,0 +1,23 @@
+// Last updated: 9/6/2026, 12:28:46 PM
+class Solution {
+public:
+    long long maximumScore(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> sMin(n);
+        sMin[n-1] = nums[n-1];
+        for(int i = n-2; i >= 0; i--) {
+            sMin[i] = min(nums[i], sMin[i+1]);
+        }
+
+        long long maxScore = numeric_limits<long long>::lowest();
+        long long windowSum = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            windowSum += nums[i];
+            long long currentScore = windowSum - sMin[i+1];
+            maxScore = max(maxScore, currentScore);
+        }
+
+        return maxScore;
+    }
+};
