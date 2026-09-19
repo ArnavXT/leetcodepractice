fd5c67d0d9556b5634d4a1c0b2dcb64377dffbe4
Arnav Tripathi <arnavtripathi21045@gmail.com>
2026-09-20 00:40:00 +0530

    Create 3871-CountCommasInRangeIi

---
+++ 3871-CountCommasInRangeIi/3871-CountCommasInRangeIi.py
@@ -0,0 +1,21 @@
+# Last updated: 9/20/2026, 12:39:58 AM
+class Solution:
+    def countCommas(self, n: int) -> int:
+        if n < 1000:
+            return 0
+
+        ans = n - 999
+
+        if n >= 1000000:
+            ans += n - 999999
+
+        if n >= 1000000000:
+            ans += n - 999999999
+
+        if n >= 1000000000000:
+            ans += n - 999999999999
+
+        if n >= 1000000000000000:
+            ans += n - 999999999999999
+
+        return ans
